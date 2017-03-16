9fb33227390ad3edd237485535f436ed061e63ab
lizzielashark <lizzie.s.sullivan@gmail.com>
2017-03-16 15:37:12 +1300

    trying connection from prev write

--- server.js
+++ server.js
@@ -12,13 +12,11 @@ var knexConfig = require('./knexfile.js')
 
 var knex = knex(knexConfig[process.env.NODE_ENV || 'development'])
 
-var pg = require('pg');
-
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres!');
-});
+var env = 'production'
+var knexConfig = require('./knexfile.js')
+var knexGenerator = require('knex')
+var knexDbConfig = knexConfig[env]
+global.knex = knexGenerator(knexDbConfig)
 
 
 
