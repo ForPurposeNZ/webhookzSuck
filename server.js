2e7b7b604f5e2cd997987efce2c7a545f2ece84c
lizzielashark <lizzie.s.sullivan@gmail.com>
2017-03-16 15:29:23 +1300

    add alt db setup to server to try and fix h10 bug

--- server.js
+++ server.js
@@ -12,13 +12,24 @@ var knexConfig = require('./knexfile.js')
 
 var knex = knex(knexConfig[process.env.NODE_ENV || 'development'])
 
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres!');
+});
+
+app.use(logger('dev'));
+app.use(cookieParser());
+
 
 app.post('/addContact', function (req, res) {
 
   payload = req.body.payload.person
 
   console.log('payload: ', payload)
-  console.log('firs name: ', payload.first_name)
+  console.log('first name: ', payload.first_name)
 
 knex('contacts').insert({ contact_name: payload.first_name })
 .then(function(data, err){
