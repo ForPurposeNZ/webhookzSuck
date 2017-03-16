0814480592271c5965404ba325e617ee5e2c6eaa
lizzielashark <lizzie.s.sullivan@gmail.com>
2017-03-16 14:42:22 +1300

    plz wrk

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser');
+var knex = require('knex')
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+
+var knexConfig = require('./knexfile.js')
+
+var knex = knex(knexConfig[process.env.NODE_ENV || 'development'])
+
+
+app.post( '/addContact', function (req, res) {
+
+knex('contacts').insert({ contact_name: 'firstnamehere' })
+.then(function(data, err){
+  if(err){
+    console.log('error message: ', err)
+  } else {
+  console.log('check data is entered')
+    }
+  })
+});
+
+
+
+
+
+
+
+app.listen(3000)
