d3fb10303b2180a6312df7d88aed196f2dfb9064
lizzielashark <lizzie.s.sullivan@gmail.com>
2017-03-20 14:27:43 +1300

    addcolumns

--- migrations/20170315133037_contacts.js
+++ migrations/20170315133037_contacts.js
@@ -1,28 +1,25 @@
-// exports.up = function(knex, Promise) {
-//   return knex.schema.createTableIfNotExists('contacts', function(table) {
-//     console.log('created contacts table!')
-//
-//
-//   table.timestamps()
-//   table.integer('contact_id')
-//   table.integer('unite_id')
-//   table.string('employer')
-//   table.string('occupation')
-//   table.string('contact_name')
-//   table.string('position')
-//   table.string('email')
-//   table.integer('phone')
-//   table.integer('phone_extension')
-//   table.integer('fax')
-//   table.integer('mobile')
-//   table.string('corresponence_contact')
-//   table.string('payroll contact')
-//   table.string('mailing_address')
-//
-//   })
-// };
-//
-// exports.down = function(knex, Promise) {
-//   console.log('droppedTable')
-//   return knex.schema.dropTableIfExists('contacts')
-// };
+exports.up = function(knex, Promise) {
+  return knex.schema.createTableIfNotExists('contacts', function(table) {
+    console.log('created contacts table!')
+
+
+  table.timestamps()
+  table.integer('contact_id')
+  table.string('contact_name')
+  table.string('position')
+  table.string('email')
+  table.integer('phone')
+  table.integer('phone_extension')
+  table.integer('fax')
+  table.integer('mobile')
+  table.string('corresponence_contact')
+  table.string('payroll contact')
+  table.string('mailing_address')
+
+  })
+};
+
+exports.down = function(knex, Promise) {
+  console.log('droppedTable')
+  return knex.schema.dropTableIfExists('contacts')
+};
--- migrations/20170320140451_droptable.js
+++ migrations/20170320140451_droptable.js
@@ -6,3 +6,19 @@ exports.up = function(knex, Promise) {
 exports.down = function(knex, Promise) {
   console.log('exports down contacts drop table')
 };
+
+
+//
+// exports.up = function(knex, Promise) {
+//   return knex.schema.table('contacts', function(table) {
+//     table.integer('unite_id')
+//     table.string('employer')
+//     table.string('occupation')
+//   })
+//
+// };
+//
+// exports.down = function(knex, Promise) {
+//   console.log('droppedTable')
+//
+// };
