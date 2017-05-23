91d6f98f171d32b137814c248791beb045e8ffa9
lizzielashark <lizzie.s.sullivan@gmail.com>
2017-05-23 15:38:18 -0700

    add correspondence contact to updatez

--- server.js
+++ server.js
@@ -84,6 +84,9 @@ app.post("/updatePerson", function(req, res) {
 
   payload = req.body.payload.person
 
+  console.log(correspondence_contact)
+  console.log("payload.primary_address.address1 :", payload.primary_address.address1)
+
   function correspondence_contact() {
      if (payload.primary_address.address1 == null ||
          payload.primary_address.address2 == null ||
