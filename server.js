3b6dcbd0617d0218eae9de35cf76ca28ce6c29ec
lizzielashark <lizzie.s.sullivan@gmail.com>
2017-05-29 15:55:57 -0700

    fixieisannoyingaf

--- server.js
+++ server.js
@@ -33,16 +33,18 @@ fixieRequest('http://www.example.com', (err, res, body) => {
   console.log(`Got response: ${res.statusCode}`);
 });
 
+const fixieConnection = new SocksConnection(mysqlServer, {
+  user: fixieValues[0],
+  pass: fixieValues[1],
+  host: fixieValues[2],
+  port: fixieValues[3],
+});
+
 console.log(process.env.FIXIE_URL)
 
 
 
 ////*** test fixie with webhookzs ***\\\
-
-fixieRequest.post('https://lastchancesaloon.herokuapp.com/addContact', function(req, res) {
- console.log("req: ", req, "res: ", res)
-
-})
 
 
 
@@ -66,6 +68,11 @@ const fixieConnection = new SocksConnection(mysqlServer, {
 });
 
 
+fixieRequest.post('https://lastchancesaloon.herokuapp.com/addContact', function (err, res, body) {
+
+  console.log("req", req, "body: ", body)
+
+)}
 
 
 
@@ -73,56 +80,56 @@ const fixieConnection = new SocksConnection(mysqlServer, {
 ////*** Add New Contact ***\\\
 
 
-fixieRequest.post('https://lastchancesaloon.herokuapp.com/addContact', function (req, res) {
-
-  console.log("req", req)
-
-  //payload = req.body.payload.person
-
- // function correspondence_contact() {
- //    if (payload.primary_address.address1 == null ||
- //        payload.primary_address.address2 == null ||
- //        payload.primary_address.address3 == null ||
- //        payload.primary_address.city == null) {
- //      return ''
- //    } else {
- //      return payload.primary_address.address1 && payload.primary_address.address2 && payload.primary_address.address3 && payload.primary_address.city
- //    }
- //  }
-
-
-  //console.log('payload: ', payload)
-  console.log('full name: ', payload.full_name)
-  console.log("id: ", payload.id)
-  // //console.log('address1', payload.primary_address.address1)
-  // //console.log('correspondence_contact: ', correspondence_contact() )
-  // // console.log(payload.full_name, 'Member_id/unite_id:', payload.unite_id, 'id:', payload.id)
-
-
-  knex('contacts').insert({
-    contact_name: payload.full_name,
-    Member_id: payload.unite_id,
-    id: payload.id,
-    employer: payload.employer,
-    occupation: payload.occupation,
-    position: payload.position,
-    Worksite_id: payload.employer,
-    email: payload.email,
-    phone: payload.phone,
-    mobile: payload.mobile,
-    //correspondence_contact: correspondence_contact(),
-    Auto_note: 1,
-    Code_id: 11,
-    Added_by: 46825
-
-  }).then(function(data, err){
-    if(err) {
-      console.log('error message: ', err)
-    } else {
-    console.log( data, payload.full_name, 'entered into SQL Database')
-      }
-    })
-   });
+// fixieRequest.post('https://lastchancesaloon.herokuapp.com/addContact', function (err, res, body) {
+//
+//   console.log("req", req, "body: ", body)
+//
+//   //payload = req.body.payload.person
+//
+//  // function correspondence_contact() {
+//  //    if (payload.primary_address.address1 == null ||
+//  //        payload.primary_address.address2 == null ||
+//  //        payload.primary_address.address3 == null ||
+//  //        payload.primary_address.city == null) {
+//  //      return ''
+//  //    } else {
+//  //      return payload.primary_address.address1 && payload.primary_address.address2 && payload.primary_address.address3 && payload.primary_address.city
+//  //    }
+//  //  }
+//
+//
+//   //console.log('payload: ', payload)
+//   console.log('full name: ', payload.full_name)
+//   console.log("id: ", payload.id)
+//   // //console.log('address1', payload.primary_address.address1)
+//   // //console.log('correspondence_contact: ', correspondence_contact() )
+//   // // console.log(payload.full_name, 'Member_id/unite_id:', payload.unite_id, 'id:', payload.id)
+//
+//
+//   knex('contacts').insert({
+//     contact_name: payload.full_name,
+//     Member_id: payload.unite_id,
+//     id: payload.id,
+//     employer: payload.employer,
+//     occupation: payload.occupation,
+//     position: payload.position,
+//     Worksite_id: payload.employer,
+//     email: payload.email,
+//     phone: payload.phone,
+//     mobile: payload.mobile,
+//     //correspondence_contact: correspondence_contact(),
+//     Auto_note: 1,
+//     Code_id: 11,
+//     Added_by: 46825
+//
+//   }).then(function(data, err){
+//     if(err) {
+//       console.log('error message: ', err)
+//     } else {
+//     console.log( data, payload.full_name, 'entered into SQL Database')
+//       }
+//     })
+//    });
 
 
 ////**** Update Person ****\\\\
