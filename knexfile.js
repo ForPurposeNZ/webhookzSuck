93670f36524a0e6a065760b77b58ea16e785018a
lizzielashark <lizzie.s.sullivan@gmail.com>
2017-05-28 17:18:51 -0700

    grr

--- knexfile.js
+++ knexfile.js
@@ -25,8 +25,8 @@ module.exports = {
     }
   },
 
-  production: 'jdbc:mysql://www.unitemembers.org.nz/unitemem_sandpit,unitemem_pitusr,Turtlemania89' || {
-    client: 'mySQL',
+  production: 'process.env.DATABASE_URL' || {
+    client: 'postgresql',
     connection: {
       database: 'my_db',
       user:     'username',
