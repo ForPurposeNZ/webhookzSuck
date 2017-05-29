498e9092a7790da923cbc56575f256715a518313
lizzielashark <lizzie.s.sullivan@gmail.com>
2017-05-29 16:10:15 -0700

    fixieisannoyingaf

--- server.js
+++ server.js
@@ -27,11 +27,17 @@ global.knex = knexGenerator(knexDbConfig)
 
 ////**** Fixie mySQL connection ****\\\\
 
-const fixieRequest = request.defaults({'proxy': process.env.FIXIE_URL});
 
-fixieRequest('http://www.example.com', (err, res, body) => {
-  console.log(`Got response: ${res.statusCode}`);
-});
+const mysql = require('mysql2');
+const SocksConnection = require('socksjs');
+
+const fixieUrl = process.env.FIXIE_SOCKS_HOST;
+const fixieValues = RegExp('[/(:\\/@)/]+');
+
+const mysqlServer = {
+  host: '50.23.215.146',
+  port: 3306
+};
 
 const fixieConnection = new SocksConnection(mysqlServer, {
   user: fixieValues[0],
@@ -40,7 +46,7 @@ const fixieConnection = new SocksConnection(mysqlServer, {
   port: fixieValues[3],
 });
 
-console.log(process.env.FIXIE_URL)
+
 
 
 
@@ -48,44 +54,18 @@ console.log(process.env.FIXIE_URL)
 
 
 
-const mysql = require('mysql2');
-const SocksConnection = require('socksjs');
-
-const fixieUrl = process.env.FIXIE_SOCKS_HOST;
-const fixieValues = fixieUrl.split(new RegExp('[/(:\\/@)/]+'));
-
-const mysqlServer = {
-  host: '50.23.215.146',
-  port: 3306
-};
-
-
-const fixieConnection = new SocksConnection(mysqlServer, {
-  user: fixieValues[0],
-  pass: fixieValues[1],
-  host: fixieValues[2],
-  port: fixieValues[3],
-});
-
-
-fixieRequest.post('https://lastchancesaloon.herokuapp.com/addContact', function (err, res, body) {
-
-  console.log("req", req, "body: ", body)
-
-)}
-
 
 
 
 ////*** Add New Contact ***\\\
 
 
-// fixieRequest.post('https://lastchancesaloon.herokuapp.com/addContact', function (err, res, body) {
-//
-//   console.log("req", req, "body: ", body)
-//
-//   //payload = req.body.payload.person
-//
+app.post('https://lastchancesaloon.herokuapp.com/addContact', function (err, res, body) {
+
+  console.log("req", req, "body: ", body)
+
+  //payload = req.body.payload.person
+
 //  // function correspondence_contact() {
 //  //    if (payload.primary_address.address1 == null ||
 //  //        payload.primary_address.address2 == null ||
@@ -99,37 +79,37 @@ fixieRequest.post('https://lastchancesaloon.herokuapp.com/addContact', function 
 //
 //
 //   //console.log('payload: ', payload)
-//   console.log('full name: ', payload.full_name)
-//   console.log("id: ", payload.id)
-//   // //console.log('address1', payload.primary_address.address1)
+  console.log('full name: ', payload.full_name)
+  console.log("id: ", payload.id)
+  console.log('address1', payload.primary_address.address1)
 //   // //console.log('correspondence_contact: ', correspondence_contact() )
 //   // // console.log(payload.full_name, 'Member_id/unite_id:', payload.unite_id, 'id:', payload.id)
-//
-//
-//   knex('contacts').insert({
-//     contact_name: payload.full_name,
-//     Member_id: payload.unite_id,
-//     id: payload.id,
-//     employer: payload.employer,
-//     occupation: payload.occupation,
-//     position: payload.position,
-//     Worksite_id: payload.employer,
-//     email: payload.email,
-//     phone: payload.phone,
-//     mobile: payload.mobile,
-//     //correspondence_contact: correspondence_contact(),
-//     Auto_note: 1,
-//     Code_id: 11,
-//     Added_by: 46825
-//
-//   }).then(function(data, err){
-//     if(err) {
-//       console.log('error message: ', err)
-//     } else {
-//     console.log( data, payload.full_name, 'entered into SQL Database')
-//       }
-//     })
-//    });
+
+
+  knex('contacts').insert({
+    contact_name: payload.full_name,
+    Member_id: payload.unite_id,
+    id: payload.id,
+    employer: payload.employer,
+    occupation: payload.occupation,
+    position: payload.position,
+    Worksite_id: payload.employer,
+    email: payload.email,
+    phone: payload.phone,
+    mobile: payload.mobile,
+    //correspondence_contact: correspondence_contact(),
+    Auto_note: 1,
+    Code_id: 11,
+    Added_by: 46825
+
+  }).then(function(data, err){
+    if(err) {
+      console.log('error message: ', err)
+    } else {
+    console.log( data, payload.full_name, 'entered into SQL Database')
+      }
+    })
+   });
 
 
 ////**** Update Person ****\\\\
