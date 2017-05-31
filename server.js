3b28def5229e6a48fb6fb3da96d7487138a813bd
lizzielashark <lizzie.s.sullivan@gmail.com>
2017-05-30 18:07:05 -0700

    stumped

--- server.js
+++ server.js
@@ -32,21 +32,21 @@ var mysql = require('mysql2')
 
 var request = require('request');
 
-var options = {
-    proxy: process.env.QUOTAGUARDSTATIC_URL,
-    url: 'http://ip.jsontest.com/',
-    headers: {
-        'User-Agent': 'node.js'
-    }
-};
-
-function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log(body);
-    }
-}
-
-request(options, callback);
+// var options = {
+//     proxy: process.env.QUOTAGUARDSTATIC_URL,
+//     url: 'http://ip.jsontest.com/',
+//     headers: {
+//         'User-Agent': 'node.js'
+//     }
+// };
+//
+// function callback(error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//         console.log(body);
+//     }
+// }
+//
+// request(options, callback);
 
 
 var url = require('url'),
