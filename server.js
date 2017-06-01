5034a5352fca432e0c927b283b4b82538b7ff0f2
lizzielashark <lizzie.s.sullivan@gmail.com>
2017-06-01 12:56:06 -0700

    trying without knex

--- server.js
+++ server.js
@@ -16,92 +16,100 @@ app.get('/', function (req, res) {
 })
 
 
-var knexConfig = require('./knexfile.js')
-
-var knex = knex(knexConfig[process.env.NODE_ENV || 'development'])
-
-var env = 'production'
-var knexConfig = require('./knexfile.js')
-var knexGenerator = require('knex')
-var knexDbConfig = knexConfig[env]
-global.knex = knexGenerator(knexDbConfig)
+// var knexConfig = require('./knexfile.js')
+//
+// var knex = knex(knexConfig[process.env.NODE_ENV || 'development'])
+//
+// var env = 'production'
+// var knexConfig = require('./knexfile.js')
+// var knexGenerator = require('knex')
+// var knexDbConfig = knexConfig[env]
+// global.knex = knexGenerator(knexDbConfig)
 
 
 ////**** QuotaGuardStatic mySQL connection ****\\\\
-//
-// var mysql = require('mysql2')
-// var url = require("url")
-// var SocksConnection = require('socksjs')
-//
-// var remote_options = {
-// host:'I put the ip address to database here',
-// port: 3306
-// };
-//
-// var proxy = url.parse(process.env.QUOTAGUARDSTATIC_URL)
-// var auth = proxy.auth;
-// var username = auth.split(":")[0]
-// var pass = auth.split(":")[1]
-//
-// var sock_options = {
-// host: proxy.hostname,
-// port: 1080,
-// user: username,
-// pass: pass
-// }
-//
-// var sockConn = new SocksConnection(remote_options, sock_options)
-// var dbConnection = mysql.createConnection({
-// user: 'I put the user here',
-// database: 'I put the database name here',
-// password: 'I put the password here',
-// stream: sockConn
-// })
-//
-// dbConnection.query('SELECT 1+1 as test1;', function(err, rows, fields) {
-//     if (err) throw err;
-//
-//     console.log('Result: ', rows)
-//     sockConn.dispose()
-//
-// })
-//
-// dbConnection.end();
-//
-//
+
+var mysql = require('mysql2')
+var url = require("url")
+var SocksConnection = require('socksjs')
+
+var remote_options = {
+host:'50.23.215.146',
+port: 3306
+};
+
+var proxy = url.parse(process.env.QUOTAGUARDSTATIC_URL)
+var auth = proxy.auth;
+var username = auth.split(":")[0]
+var pass = auth.split(":")[1]
+
+var sock_options = {
+host: proxy.hostname,
+port: 1080,
+user: username,
+pass: pass
+}
+
+var sockConn = new SocksConnection(remote_options, sock_options)
+var dbConnection = mysql.createConnection({
+user: 'unitemem_pituser',
+database: 'unitemem_sandpit',
+password: 'Du1s58@@3',
+stream: sockConn
+})
+
+dbConnection.query('SELECT 1+1 as test1;', function(err, rows, fields) {
+    if (err) throw err;
+    console.log('Result: ', rows)
+
+    payload = req.body.payload.person
+
+  INSERT INTO contacts (contact_name, Member_id)
+  VALUES (payload.full_name, payload.unite_id);
+
+
+
+
+    sockConn.dispose()
+
+})
+
+dbConnection.end();
+
+
 
 ////*** Add New Contact ***\\\
 
 
-app.post('/addContact', function (req, res) {
-
-  payload = req.body.payload.person
-  console.log('full name: ', payload.full_name)
-  console.log("id: ", payload.id)
-
-  knex('contacts').insert({
-    contact_name: payload.full_name,
-    Member_id: payload.unite_id,
-    id: payload.id,
-    employer: payload.employer,
-    occupation: payload.occupation,
-    position: payload.position,
-    Worksite_id: payload.employer,
-    email: payload.email,
-    phone: payload.phone,
-    mobile: payload.mobile,
-    Auto_note: 1,
-    Code_id: 11,
-    Added_by: 46825
-
-  }).then(function(data, err){
-    if(err) {
-      console.log('error message: ', err)
-    } else {
-    console.log( data, payload.full_name, 'entered into SQL Database')
-      }
-    })
-  });
+// app.post('/addContact', function (req, res) {
+//
+//   payload = req.body.payload.person
+//   console.log('full name: ', payload.full_name)
+//   console.log("id: ", payload.id)
+//
+//   knex('contacts').insert({
+//     contact_name: payload.full_name,
+//     Member_id: payload.unite_id,
+//     id: payload.id,
+//     employer: payload.employer,
+//     occupation: payload.occupation,
+//     position: payload.position,
+//     Worksite_id: payload.employer,
+//     email: payload.email,
+//     phone: payload.phone,
+//     mobile: payload.mobile,
+//     Auto_note: 1,
+//     Code_id: 11,
+//     Added_by: 46825
+//
+//   }).then(function(data, err){
+//     if(err) {
+//       console.log('error message: ', err)
+//     } else {
+//     console.log( data, payload.full_name, 'entered into SQL Database')
+//       }
+//     })
+//   });
 
 
 app.listen(port)
