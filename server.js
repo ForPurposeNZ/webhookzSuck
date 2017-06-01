cf3c73e36b3e3f6b25c61e673f669add89d64794
lizzielashark <lizzie.s.sullivan@gmail.com>
2017-05-31 20:43:36 -0700

    ting

--- server.js
+++ server.js
@@ -24,29 +24,6 @@ var knexGenerator = require('knex')
 var knexDbConfig = knexConfig[env]
 global.knex = knexGenerator(knexDbConfig)
 
-//////////////////////////////////////
-
-
-var request = require('request');
-
-var options = {
-    proxy: process.env.QUOTAGUARDSTATIC_URL,
-    url: 'http://ip.jsontest.com/',
-    headers: {
-        'User-Agent': 'node.js'
-    }
-};
-
-function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log(body);
-    }
-}
-
-request(options, callback);
-
-
-
 ////**** QuotaGuardStatic mySQL connection ****\\\\
 
 var mysql = require('mysql2')
@@ -54,8 +31,8 @@ var url = require("url")
 var SocksConnection = require('socksjs')
 
 var remote_options = {
-host:'50.23.215.146',
-port: 3306
+  host:'50.23.215.146',
+  port: 3306
 };
 
 var proxy = url.parse(process.env.QUOTAGUARDSTATIC_URL)
@@ -64,61 +41,25 @@ var username = auth.split(":")[0]
 var pass = auth.split(":")[1]
 
 var sock_options = {
-host: proxy.hostname,
-port: 1080,
-user: username,
-pass: pass
+  host: proxy.hostname,
+  port: 1080,
+  user: username,
+  pass: pass
 }
 
 var sockConn = new SocksConnection(remote_options, sock_options)
 var dbConnection = mysql.createConnection({
-user: 'unitemem_pituser',
-database: 'unitemem_sandpit',
-password: 'Du1s58@@3',
-stream: sockConn
+  user: 'unitemem_pituser',
+  database: 'unitemem_sandpit',
+  password: 'Du1s58@@3',
+  stream: sockConn
 })
 
 dbConnection.query('SELECT 1+1 as test1;', function(err, rows, fields) {
+  if (err) throw err;
 
-
-  app.post('/addContact', function (req, res) {
-
-    payload = req.body.payload.person
-    console.log('full name: ', payload.full_name)
-    console.log("id: ", payload.id)
-
-    knex('contacts').insert({
-      contact_name: payload.full_name,
-      Member_id: payload.unite_id,
-      id: payload.id,
-      employer: payload.employer,
-      occupation: payload.occupation,
-      position: payload.position,
-      Worksite_id: payload.employer,
-      email: payload.email,
-      phone: payload.phone,
-      mobile: payload.mobile,
-      Auto_note: 1,
-      Code_id: 11,
-      Added_by: 46825
-
-    }).then(function(data, err){
-      if(err) {
-        console.log('error message: ', err)
-      } else {
-      console.log( data, payload.full_name, 'entered into SQL Database')
-        }
-      })
-     });
-
-
-
-
-
-if (err) throw err;
-
-console.log('Result: ', rows)
-sockConn.dispose()
+  console.log('Result: ', rows)
+  sockConn.dispose()
 
 })
 
@@ -129,119 +70,40 @@ dbConnection.end();
 ////*** Add New Contact ***\\\
 
 
-// app.post('/addContact', function (req, res) {
-//
-//   console.log('fuck this shit')
-//
-//   payload = req.body.payload.person
-//
-// //  // function correspondence_contact() {
-// //  //    if (payload.primary_address.address1 == null ||
-// //  //        payload.primary_address.address2 == null ||
-// //  //        payload.primary_address.address3 == null ||
-// //  //        payload.primary_address.city == null) {
-// //  //      return ''
-// //  //    } else {
-// //  //      return payload.primary_address.address1 && payload.primary_address.address2 && payload.primary_address.address3 && payload.primary_address.city
-// //  //    }
-// //  //  }
-// //
-// //
-// //   //console.log('payload: ', payload)
-//   console.log('full name: ', payload.full_name)
-//   console.log("id: ", payload.id)
-//   // console.log('address1', payload.primary_address.address1)
-// // //   // //console.log('correspondence_contact: ', correspondence_contact() )
-// // //   // // console.log(payload.full_name, 'Member_id/unite_id:', payload.unite_id, 'id:', payload.id)
-// //
-// //
-//   knex('contacts').insert({
-//     contact_name: payload.full_name,
-//     Member_id: payload.unite_id,
-//     id: payload.id,
-//     employer: payload.employer,
-//     occupation: payload.occupation,
-//     position: payload.position,
-//     Worksite_id: payload.employer,
-//     email: payload.email,
-//     phone: payload.phone,
-//     mobile: payload.mobile,
-//     //correspondence_contact: correspondence_contact(),
-//     Auto_note: 1,
-//     Code_id: 11,
-//     Added_by: 46825
-//
-//   }).then(function(data, err){
-//     if(err) {
-//       console.log('error message: ', err)
-//     } else {
-//     console.log( data, payload.full_name, 'entered into SQL Database')
-//       }
-//     })
-//    });
+app.post('/addContact', function (req, res) {
 
-
-////**** Update Person ****\\\\
-
-// app.post("/updatePerson", function(req, res) {
-//
-//   payload = req.body.payload.person
-//
-//   console.log(payload.primary_address.address1)
-//
-//   function correspondence_contact() {
-//      if (payload.primary_address.address1 == null) {
-//        console.log("null")
-//      } else {
-//        console.log(payload.primary_address.address1)
-//    }
-//  }
- //
- //  knex('contacts').where({id: payload.id}).update({
- //    contact_name: payload.full_name,
- //    Member_id: payload.unite_id,
- //    id: payload.id,
- //    employer: payload.employer,
- //    occupation: payload.occupation,
- //    position: payload.position,
- //    Worksite_id: payload.employer,
- //    email: payload.email,
- //    phone: payload.phone,
- //    mobile: payload.mobile,
- //    correspondence_contact: correspondence_contact(),
- //    Auto_note: 1,
- //    Code_id: 10,
- //    Added_by: 46825
- //
- //    })
- //    .then(function(data, err){
- //      if(err){
- //        console.log('error message: ', err)
- //      } else {
- //      console.log('Person Successfully Updated from Nationbuilder to SQL')
- //      }
- //  })
-//})
-
-
-////**** Delete a Person ****\\\\
-
-
-app.post("/deletePerson", function(req, res) {
 
   payload = req.body.payload.person
 
-  knex('contacts').where({unite_id: payload.unite_id}).del()
-  .then(function(data, err){
-    if(err){
+
+
+  console.log('full name: ', payload.full_name)
+  console.log("id: ", payload.id)
+
+
+  knex('contacts').insert({
+    contact_name: payload.full_name,
+    Member_id: payload.unite_id,
+    id: payload.id,
+    employer: payload.employer,
+    occupation: payload.occupation,
+    position: payload.position,
+    Worksite_id: payload.employer,
+    email: payload.email,
+    phone: payload.phone,
+    mobile: payload.mobile,
+    Auto_note: 1,
+    Code_id: 11,
+    Added_by: 46825
+
+  }).then(function(data, err){
+    if(err) {
       console.log('error message: ', err)
     } else {
-    console.log('Person Deleted. Goodbye.')
-    }
-  })
-})
-
-
+    console.log( data, payload.full_name, 'entered into SQL Database')
+      }
+    })
+   });
 
 
 app.listen(port)
