a0596333cae006ed2f2683a4b19b034fb51dd0e6
lizzielashark <lizzie.s.sullivan@gmail.com>
2017-06-12 14:35:11 -0700

    still peskilating sockconndispose

--- server.js
+++ server.js
@@ -170,7 +170,6 @@ app.post('/addContact', function (req, res) {
 
 
   })
-      sockConn.dispose();
 
   // dbConnection.query('INSERT INTO ' + extInfoUniteTable + ' SET ?', memberNotesData, function(err, rows, fields) {
   //       if (err) throw err;
