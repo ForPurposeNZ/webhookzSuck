3539d3b04152ec55d2831ec2f211cdb38c5b9432
lizzielashark <lizzie.s.sullivan@gmail.com>
2017-07-04 13:54:55 -0700

    added  connectionLimit: 10,     acquireTimeout: 30000, //30 secs"

--- server.js
+++ server.js
@@ -46,7 +46,9 @@ var dbConnection = mysql.createConnection({
 user: process.env.DB_USER,
 database: process.env.DB_DATABASE,
 password: process.env.DB_PASSWORD,
-stream: sockConn
+stream: sockConn,
+connectionLimit: 10,
+acquireTimeout: 30000 //30 secs
 })
 
 
