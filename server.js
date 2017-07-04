9d3678ea1e3edfcb429c5e1b4f7f8180e0f12f67
lizzielashark <lizzie.s.sullivan@gmail.com>
2017-07-04 16:02:30 -0700

    changed acquire timeout to connectTimeout

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ var sock_options = {
 var sockConn = new SocksConnection(remote_options, sock_options)
 
 var dbConnection = mysql.createConnection({
-acquireTimeout: 2000000,
+connectTimeout: 2000000,
 user: process.env.DB_USER,
 database: process.env.DB_DATABASE,
 password: process.env.DB_PASSWORD,
